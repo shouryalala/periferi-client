29b178ddf695e08a0fed7b68f985bc5dd910a8dd
shouryalala <shouryalala@gmail.com>
2017-05-19 12:28:04 +0530

    successfull send receive requests using volley

--- app/src/main/java/com/eightyeightysix/shourya/almondclient/FeedActivity.java
+++ app/src/main/java/com/eightyeightysix/shourya/almondclient/FeedActivity.java
@@ -5,11 +5,31 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class FeedActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button button;
+    RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +46,41 @@ public class FeedActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        //Request Queue
+        queue = Volley.newRequestQueue(this);
+
+
+        editText = (EditText)findViewById(R.id.editText1);
+        button = (Button)findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendRequest();
+            }
+        });
+    }
+
+    public void sendRequest() {
+        String name = editText.getText().toString();
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("name", name);
+        String url = "http://192.168.0.104:3000/post/add_entry/"+ name;
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, new JSONObject(params), new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Toast.makeText(getApplicationContext(), response.toString(), Toast.LENGTH_LONG).show();
+                Log.d("Almond",response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Almond","Volley Response fetch error");
+                error.printStackTrace();
+            }
+        });
+        queue.add(jsonObjectRequest);
     }
 
     @Override
